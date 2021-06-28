49f81898ec3ea280bed053aa225f96434159d0d3
MATTHEW THERAULT <MATTHEW.THERAULT@LibertyMutual.com>
2021-06-28 14:19:49 -0400

    Finished numbers.js exercise

--- assets/javascript/numbers.js
+++ assets/javascript/numbers.js
@@ -1,2 +1,13 @@
 console.log("numbers.js");
 let myNum = 1;
+
+myNum += 15;
+console.log(`Number after adding 15: ${myNum}`)
+myNum -= 8;
+console.log(`Number after subtracting 8, but not using the variable: ${myNum}`)
+let avgVar = (26 + 44 + 72 + 86 + 112) / 5;
+console.log(`Average Variable output: ${avgVar}`)
+
+let theNumber = 14;
+console.log(theNumber + 1)
+console.log (`The variable is a: ${typeof theNumber}`)

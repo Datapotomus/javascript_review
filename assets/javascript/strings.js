bc981b5635374af408c6c3559b0d0aa2b8b38b78
MATTHEW THERAULT <MATTHEW.THERAULT@LibertyMutual.com>
2021-06-28 15:46:30 -0400

    finished with string literals.

---
+++ assets/javascript/strings.js
@@ -0,0 +1,27 @@
+console.log("strings.js");
+
+// #1 Creating 5 variables
+let verb = "run";
+let noun = "house";
+let adjective = "damp";
+let color = "green";
+let nameVar = "Matt";
+
+// #2 creating madlib variable
+let madlib = nameVar + " will not " + verb + ". \n" + "Unless, you clean up the " + adjective +  " " + color + " " + noun + "."
+
+console.log(madlib)
+
+// #3 creating madlibLiteral variable, and using ticks
+let madlibLiteral = `${nameVar} will not ${verb}.
+Unless, you clean up the ${adjective} ${color} ${noun}.`
+
+console.log(madlibLiteral)
+
+// #4 You can use the .toString function.
+
+let numTest = 4.
+
+console.log(typeof numTest)
+console.log(typeof numTest.toString())
+

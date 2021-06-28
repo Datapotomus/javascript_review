b72ebd6fb9d399d0d803145cdd69d133ba068143
MATTHEW THERAULT <MATTHEW.THERAULT@LibertyMutual.com>
2021-06-28 14:30:46 -0400

    finishing polish numbers activity.

--- assets/javascript/numbers.js
+++ assets/javascript/numbers.js
@@ -9,5 +9,5 @@ let avgVar = (26 + 44 + 72 + 86 + 112) / 5;
 console.log(`Average Variable output: ${avgVar}`)
 
 let theNumber = 14;
-console.log(theNumber + 1)
+console.log(++theNumber)
 console.log (`The variable is a: ${typeof theNumber}`)
